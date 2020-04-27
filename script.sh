a913f30fbbe3cea6a014034fe709f5fa7e8f77f0
Angelo <angelopitarresi1@gmail.com>
2020-04-27 11:24:16 -0400

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+var1=HTTP
+echo podaj nazwe testowanego protokolu:
+read protokol
+
+echo Podaj adres IP celu:
+read IP
+echo ile razy wykonac test?
+read END
+START=1
+service tor start
+
+if [ "${protokol,,}" = "${var1,,}" ]; 
+then
+echo Podaj adres uri formularza logowania:
+read URI
+echo Podaj nazwe parametru "login":
+read LOGIN
+echo Podaj nazwe parametru "haslo":
+read PASS
+echo Podaj komunikat nieudanego logowania:
+read KOMUNIKAT
+for i in $(eval echo "{$START..$END}")
+do
+proxychains hydra -l test -p test -vV -F $IP http-post-form "$URI:$LOGIN=^USER^&$PASS=^PASS^:F=$KOMUNIKAT"
+service tor reload
+done
+else
+for i in $(eval echo "{$START..$END}")
+do
+proxychains hydra -l test -p test -vV -F $IP ftp
+service tor reload
+done
+fi
